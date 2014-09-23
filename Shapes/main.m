12f8c7610fcd46c75358e5f5b0fc5fd63e1f2b0c
roshan <roshan.prakash.r@gmail.com>
2014-09-23 16:35:40 +0100

    Initial Commit

---
+++ Shapes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Shapes
+//
+//  Created by roshan on 23/09/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
